8068a21713193f66d12f3ae3978deb0bd4ea5afb
think <github7S7>
2019-03-23 08:58:47 +0800

    spring-class init

---
+++ src/main/java/edu/nsu/cloud/springclass/SpringClassApplication.java
@@ -0,0 +1,13 @@
+package edu.nsu.cloud.springclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClassApplication.class, args);
+    }
+
+}
